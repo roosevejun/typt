f89ed0ed76d03ff78d3c34cfac7bb5d92460cb5e
roosevejun <roosevejun@gmail.com>
2017-04-01 19:43:13 +0800

    框架升级

--- typt-common/src/main/java/com/smooth/typt/common/dao/CustomBaseSqlDaoImpl.java
+++ typt-common/src/main/java/com/smooth/typt/common/dao/CustomBaseSqlDaoImpl.java
@@ -1,7 +1,6 @@
 package com.smooth.typt.common.dao;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -9,109 +8,13 @@ import java.util.regex.Pattern;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-
 import com.smooth.typt.common.entity.PageModel;
-import org.hibernate.SQLQuery;
-import org.hibernate.criterion.CriteriaSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustomBaseSqlDaoImpl {
 	
 	@Autowired
 	private EntityManager em;
-	
-	public List<Map<String, Object>> querySqlObjects(String sql, Integer currentPage,Integer rowsInPage){
-		return this.querySqlObjects(sql, null, currentPage, rowsInPage);
-	}
-	
-	public List<Map<String, Object>> querySqlObjects(String sql){
-		return this.querySqlObjects(sql, null, null, null);
-	}
-	
-	public List<Map<String, Object>> querySqlObjects(String sql, List<Object> params){
-		return this.querySqlObjects(sql, params, null, null);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<Map<String, Object>> querySqlObjects(String sql, Object params, Integer currentPage,Integer rowsInPage){
-		Query qry = em.createNativeQuery(sql);
-		SQLQuery s = qry.unwrap(SQLQuery.class);
-		
-		//设置参数
-		if(params != null){
-			if(params instanceof List){
-				List<Object> paramList = (List<Object>) params;
-				for(int i = 0, size = paramList.size(); i < size; i++){
-					qry.setParameter(i+1, paramList.get(i));
-				}
-			}else if(params instanceof Map){
-				Map<String, Object> paramMap = (Map<String, Object>) params;
-				for(String key : paramMap.keySet()){
-					qry.setParameter(key, paramMap.get(key));
-				}
-			}
-		}
-		
-		if (currentPage != null && rowsInPage != null) {//判断是否有分页
-			// 起始对象位置
-			qry.setFirstResult(rowsInPage * (currentPage - 1));
-			// 查询对象个数
-			qry.setMaxResults(rowsInPage);
-		}
-		s.setResultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP);
-		List<Map<String, Object>> resultList=new ArrayList<Map<String, Object>>();
-		try {
-			resultList=s.list();
-		} catch (Exception e) {
-		}finally{
-			em.close();
-		}
-		return resultList;
-	}
-	
-	public PageModel<Map<String, Object>> querySqlObjects(String sql,
-														  String sbCount, Map<String, Object> params, Integer currentPage, Integer rowsInPage){
-		PageModel<Map<String, Object>> pageModel = new PageModel<Map<String, Object>>();
-		
-		List<Map<String, Object>> list = this.querySqlObjects(sql, params, currentPage, rowsInPage);
-
-		pageModel.setList(list);
-		
-		if(currentPage == null || rowsInPage == null){
-			pageModel.setTotalCount(list == null ? 0 : list.size());
-		}else{
-			Integer count = this.queryCountBySql(sbCount, params);
-			
-			pageModel.setCurrentPage(currentPage);
-			pageModel.setTotalCount(count);
-			pageModel.setPageSize(rowsInPage);
-			
-			int totalPage = 0;
-			if(count%rowsInPage == 0){
-				totalPage = count / rowsInPage;
-			}else{
-				totalPage = count / rowsInPage + 1;
-			}
-			
-			pageModel.setTotalPage(totalPage);
-		}
-		
-		
-		return pageModel;
-	}
-	
-	
-	public int getCount(String sql){
-		String sqlCount="select count(0) count_num from ("+sql+") as total";
-		List<Map<String, Object>> list = this.querySqlObjects(sqlCount);
-		if(list.size() > 0){
-			int countNum=((BigInteger) list.get(0).get("count_num")).intValue();
-			return countNum;
-		}else{
-			return 0;
-		}
-	}
-	
 	/**
 	 * 处理sql语句
 	 * 
@@ -351,97 +254,7 @@ public class CustomBaseSqlDaoImpl {
 		return page;
 	}
 
-        /**
-	 * 根据SQL语句查询分页
-	 * @param sql
-	 * @param currentPage
-	 * @param pageSize
-	 * @return
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public PageModel queryForPageBySql(String sql,Integer currentPage,Integer pageSize){
-		PageModel page = new PageModel();
-		Integer totalCount = 0;
-		Integer totalPage = 0;
-		
-		//EntityManager em = this.emf.createEntityManager();
-		Query qry = em.createNativeQuery(sql);
-		Query qry2 = em.createNativeQuery(sql);
-		SQLQuery s = qry.unwrap(SQLQuery.class);
-		if (currentPage != null && pageSize != null) {//判断是否有分页
-			// 起始对象位置
-			qry.setFirstResult(pageSize * (currentPage - 1));
-			// 查询对象个数
-			qry.setMaxResults(pageSize);
-		}
-		s.setResultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP);
-		List<Map> resultList=new ArrayList<Map>();
-		List list = qry2.getResultList();
-		totalCount = (list == null ? 0 : list.size());
-		if(totalCount % pageSize == 0){
-			totalPage = totalCount/pageSize;
-		}else{
-			totalPage = totalCount/pageSize + 1;
-		}
-		try {
-			resultList=s.list();
-			page.setCurrentPage(currentPage);
-			page.setPageSize(pageSize);
-			page.setList(resultList);
-			page.setTotalCount(totalCount);
-			page.setTotalPage(totalPage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally{
-			em.close();
-		}		
-		return page;
-	}
-	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public PageModel queryForPageBySql(String sql,Map<String,Object> params,Integer currentPage,Integer pageSize){
-		PageModel page = new PageModel();
-		Integer totalCount = 0;
-		Integer totalPage = 0;
-		
-		Query qry = em.createNativeQuery(sql);
-		Query qry2 = em.createNativeQuery(sql);
-		
-		for(Map.Entry<String,Object> entry: params.entrySet()){
-			qry.setParameter(entry.getKey(), entry.getValue());
-			qry2.setParameter(entry.getKey(), entry.getValue());
-		}
-		
-		SQLQuery s = qry.unwrap(SQLQuery.class);
-		if (currentPage != null && pageSize != null) {//判断是否有分页
-			// 起始对象位置
-			qry.setFirstResult(pageSize * (currentPage - 1));
-			// 查询对象个数
-			qry.setMaxResults(pageSize);
-		}
-		s.setResultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP);
-		List<Map> resultList=new ArrayList<Map>();
-		List list = qry2.getResultList();
-		totalCount = (list == null ? 0 : list.size());
-		if(totalCount % pageSize == 0){
-			totalPage = totalCount/pageSize;
-		}else{
-			totalPage = totalCount/pageSize + 1;
-		}
-		try {
-			resultList=s.list();
-			page.setCurrentPage(currentPage);
-			page.setPageSize(pageSize);
-			page.setList(resultList);
-			page.setTotalCount(totalCount);
-			page.setTotalPage(totalPage);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally{
-			em.close();
-		}		
-		return page;
-	}
+
 	
 	/**
 	 * 查询总数
@@ -471,7 +284,7 @@ public class CustomBaseSqlDaoImpl {
 	
 	/**
 	 * 查询总数
-	 * @param hql
+	 * @param sql
 	 * @param params
 	 * @return
 	 */
